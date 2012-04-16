bb6e5fe51614e6404ace06ddde997ddb6a65d267
Amar Chadgar <i@amarchadgar.com>
2012-04-17 01:51:49 +0530

    too many updates

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>TITLE</title>
+		<title></title>
 		<link rel="shortcut icon" href="images/icon.png" type="text/png" />
 		<link rel="stylesheet" href="styles/style.css" type="text/css">
 		<link rel="stylesheet" href="styles/grid.css" type="text/css">
 		<script type="text/javascript" src="scripts/jquery.min.js"></script>
 		<script type="text/javascript" src="scripts/script.js"></script>
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]--> 
 	</head>
 	<body>
 	  
