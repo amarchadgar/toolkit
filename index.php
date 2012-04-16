599c0c4340c52101d927ce250e80aca379ed2b7e
Amar Chadgar <i@amarchadgar.com>
2012-04-17 01:08:12 +0530

    index updated

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>TITLE</title>
+		<link rel="shortcut icon" href="images/icon.png" type="text/png" />
+		<link rel="stylesheet" href="styles/style.css" type="text/css">
+		<link rel="stylesheet" href="styles/grid.css" type="text/css">
+		<script type="text/javascript" src="jquery.min.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+	  
+	</body>
+</html>
