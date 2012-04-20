e5546ce6169da59410af47c0b1419b61f9d43a8a
Amar Chadgar <i@amarchadgar.com>
2012-04-20 22:25:13 +0530

    cdn hosted copy and fallback for it added

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 		<link rel="shortcut icon" href="images/icon.png" type="text/png" />
 		<link rel="stylesheet" href="styles/style.css" type="text/css">
 		<link rel="stylesheet" href="styles/grid.css" type="text/css">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="scripts/jquery.min.js"><\/script>')</script>
 		<script type="text/javascript" src="scripts/jquery.min.js"></script>
 		<script type="text/javascript" src="scripts/script.js"></script>
 		<!--[if lt IE 9]>
