0b8ea0e279eb28fd5c2479adbcd9800f2fd664b2
Amar Chadgar <i@amarchadgar.com>
2012-04-20 22:53:25 +0530

    cdn hosted copy and fallback for it added

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 		<link rel="stylesheet" href="styles/grid.css" type="text/css">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="scripts/jquery.min.js"><\/script>')</script>
-		<script type="text/javascript" src="scripts/jquery.min.js"></script>
 		<script type="text/javascript" src="scripts/script.js"></script>
 		<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
